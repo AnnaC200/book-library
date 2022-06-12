817de9a5676ff420718da00a5fab6614cbee8b5b
Anna Chan <annachan200@gmail.com>
2022-06-12 11:35:02 +0100

    issue with posttest script

--- src/models/reader.test.js
+++ src/models/reader.test.js
@@ -1,5 +1,26 @@
-const { Reader } = require('../models')
+const { expect } = require('chai');
+const request = require('supertest');
+const { Reader } = require('../src/models');
+const app = require('../src/app');
 
 describe('/readers', () => {
-    
-})
+// func to run once before ALL the test in describe
+  before(async () => Reader.sequelize.sync());
+// func to run before EACH test in describe to clean out the Reader table so there's no interference from previous tests
+  beforeEach(async () => {
+      await Reader.destroy({ where: {} });
+  })
+// creating a new reader by sending data to the /readers endpoint, asserting a new record gets added in the READERS table in DB
+  describe('with no records in the database', () => {
+    describe('POST /readers', () => {
+      it('creates a new reader in the database', async () => {
+        const response = await request(app).post('/readers').send({
+          name: 'Elizabeth Bennet',
+          email: 'future_ms_darcy@gmail.com',
+        });
+
+        expect(response.status).to.equal(201);
+      });
+    });
+  });
+});
